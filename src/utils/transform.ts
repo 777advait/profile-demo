1f3eb769c9833b11664de75b6d39ca0bca49e5b4
Advait <76993204+777advait@users.noreply.github.com>
2025-04-10 21:13:15 +0530

    fix: some more minor lint fixes

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -1,14 +1,14 @@
-export function cleanNulls<T extends Record<string, any>>(
+export function cleanNulls<T extends Record<string, unknown>>(
   obj: T,
 ): {
   [K in keyof T]: T[K] extends null ? undefined : T[K];
 } {
-  const result: any = {};
+  const result: Partial<T> = {};
 
   for (const key in obj) {
     const value = obj[key];
     result[key] = value ?? undefined;
   }
 
-  return result;
+  return result as { [K in keyof T]: T[K] extends null ? undefined : T[K] };
 }
